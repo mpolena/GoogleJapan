78e8a4ebb6de4ec7ba509cceb3eb199a69a6679b
mikel polena <mpolena@hawk.iit.edu>
2022-04-02 13:49:22 -0500

    slight code improvements and logging.

--- google_japan.py
+++ google_japan.py
@@ -1,5 +1,7 @@
 import argparse
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 def check_straight(lst):
     '''Checks if parameter passed in is a straight. Returns T/F.'''
@@ -15,6 +17,7 @@ def straight(lst):
     '''Checks if passed list contains one or more straights.'''
     
     if len(lst)%5 != 0:
+        logging.info(f"Number of elements in {lst} is not divisible by 5")
         return False
     lst.sort()
     
@@ -30,26 +33,29 @@ def straight(lst):
     try:
         assert(len(first_list) == 5)
     except AssertionError:
+        logging.info(f"Number of elements in {first_list} is not divisible by 5")
         return False
     
     # if check_straight returns False, done
     if not check_straight(first_list):
+        logging.info("Sorry, not a straight.")
         return False
     
     # else recurse with remaining list.
     elif len(rest_lst) >= 5:
         return straight(rest_lst)
     
+    logging.info("Straight!")
     return True
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser("Determine if a list of integers is a straight.")
+    parser = argparse.ArgumentParser("Determine if a list of integers contains only straights.")
     parser.add_argument('-l','--list', nargs='+', type=int, help='<Required> Set flag', required=True)
-    
     for _, value in parser.parse_args()._get_kwargs():
         if value is not None:
             lst = value
     assert(type(lst)==list)
     
-    print(lst, straight(lst))
+    print(lst)
+    straight(lst)
